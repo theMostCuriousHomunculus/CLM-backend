24443fdb36eb9a28847294ca766d5e5750356d21
Casey Johnson <johnson.casey1211@gmail.com>
2022-04-11 14:28:42 -0500

    chore: saving work

--- src/GraphQL/resolvers/account/field/conversations.ts
+++ src/GraphQL/resolvers/account/field/conversations.ts
@@ -1,4 +1,5 @@
 import Account from '../../../../types/interfaces/Account';
+import CLMRequest from '../../../../types/interfaces/CLMRequest';
 import ConversationModel from '../../../../mongodb/models/conversation.js';
 
 interface ConversationsArgs {
@@ -6,13 +7,22 @@ interface ConversationsArgs {
   skip?: number;
 }
 
-export default async function (parent: Account, args: ConversationsArgs) {
-  if (Number.isInteger(args.limit) && Number.isInteger(args.skip)) {
-    const { limit, skip } = args;
-    return await ConversationModel.find({ participants: parent._id })
-      .skip(skip!)
-      .limit(limit!);
+export default async function (
+  parent: Account,
+  args: ConversationsArgs,
+  context: CLMRequest
+) {
+  const { bearer } = context;
+  if (!bearer || bearer._id.toString() !== parent._id.toString()) {
+    return null;
   } else {
-    return await ConversationModel.find({ participants: parent._id });
+    if (Number.isInteger(args.limit) && Number.isInteger(args.skip)) {
+      const { limit, skip } = args;
+      return await ConversationModel.find({ participants: parent._id })
+        .skip(skip!)
+        .limit(limit!);
+    } else {
+      return await ConversationModel.find({ participants: parent._id });
+    }
   }
 }
